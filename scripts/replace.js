d194e591d753cea42b089c0e97ab88db94c61135
JacobLinCool <jacoblincool@gmail.com>
2024-11-05 14:19:28 +0800

    build: cf pages

---
+++ scripts/replace.js
@@ -0,0 +1,8 @@
+import fs from 'node:fs';
+
+const workerCode = fs.readFileSync('.svelte-kit/cloudflare/_worker.js', 'utf-8');
+const newWorkerCode = workerCode.replace(
+    /requireNode\(\);/,
+    'requireBrowser();'
+);
+fs.writeFileSync('.svelte-kit/cloudflare/_worker.js', newWorkerCode);
